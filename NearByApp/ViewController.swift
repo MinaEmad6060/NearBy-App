c987bd3d3c94f26f5961c5bf2129a823b0b669c4
MinaEmad6060 <minaemad6060@gmail.com>
2024-05-22 05:32:15 -0700

    create nearLocations tableView

--- NearByApp/ViewController.swift
+++ NearByApp/ViewController.swift
@@ -7,11 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var nearLocationsTableView: UITableView!
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        nearLocationsTableView.delegate = self
+        nearLocationsTableView.dataSource = self
         // Do any additional setup after loading the view.
+//        let nibCustomCell = UINib(nibName: "CustomTableViewCell", bundle: nil)
+//        tableViewNib.register(nibCustomCell, forCellReuseIdentifier: "customCell")
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath)
+//        as! CustomTableViewCell
+        
+        cell.textLabel?.text = "hello"
+        
+        
+        return cell
     }
 
 
